604898ccef6fb8d98c976b44c0fb263fff43692b
bobbykemp <robert.kemp@mavs.uta.edu>
2018-10-30 23:39:49 -0500

    Initial commit

---
+++ Assets/Inventory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        List<Item> items = new List<Item>();
+
+        Item map = new Item("Map", "Permanent", 1);
+        Item sword = new Item("Sword", "Collectible", 1);
+        Item shield = new Item("Shield", "Collectible", 1);
+        Item jawbone = new Item("Jawbone", "Magical", 1);
+
+        items.Add(map);
+        items.Add(sword);
+        items.Add(shield);
+        items.Add(jawbone);
+
+        foreach(Item i in items) {
+            print(i.name + " " + i.type + " " + i.quantity);
+        }
+
+        items.Sort();
+
+        foreach (Item i in items) {
+            print(i.name + " " + i.type + " " + i.quantity);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class Item : IComparable<Item> {
+
+    public string name;
+    public string type;
+    public int quantity;
+
+    public Item(string _name, string _type, int _quantity) {
+        name = _name;
+        type = _type;
+        quantity = _quantity;
+    }
+
+    public int CompareTo(Item other) {
+        /*this method will tell how to sort the objects
+        
+        return < 0 - this instance comes before "other" in the sort
+        return 0 - this instance occurs in the same position in the sort as other
+        return > 0 - this instance occurs after "other" in the sort
+
+        See https://docs.microsoft.com/en-us/dotnet/api/system.icomparable.compareto?view=netframework-4.7.2
+        for more details
+
+        */
+
+        //if the thing we're comparing to is null, then put this item after other in the sort
+        if (other == null) {
+            return 1;
+        }
+
+        //sort by name string
+        return String.Compare(name, other.name);
+    }
+}
